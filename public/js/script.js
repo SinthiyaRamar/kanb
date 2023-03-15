b5056accdc11a95e852bd4ceb560c5d151ba46b0
SinthiyaRamar <sinthiyasinthiya249@gmail.com>
2023-03-15 12:02:53 +0530

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -8,40 +8,82 @@ window.addEventListener("load", function () {
     
     // $("#slidebar").append("<div id=board ></div>")
     // $("#slidebar").append("<div id=theme ></div>")
-    $("#theme").append("<div id=selectTheme><img class=logo src=./assets/icon-dark-theme.svg /><input id=checkbox type=checkbox />  <img class=logo src=./assets/icon-light-theme.svg </div>")
-    $("#theme").append("<div id=hideSidebar class=hideSideBar > <img class=hide src=./assets/icon-hide-sidebar.svg /> Hide Sidebar </div>")
+    $("#theme").append("<div id=selectTheme><img class=logo src=./assets/icon-dark-theme.svg /><input id=checkbox  type=checkbox />  <img class=logo src=./assets/icon-light-theme.svg </div>")
+    $("#theme").append("<div id=hideSidebar class=hideSideBar hide > <img class=hide hideSideBar src=./assets/icon-hide-sidebar.svg /> Hide Sidebar </div>")
     
 
     $("#checkbox").click(function(){
         if($("#checkbox").is(":checked")){
-            ($("#whole-container").attr("id","WholeContainer"));
-            ($("#header").attr("id","HeaderDark"));
-            ($("#slidebar").attr("id","slidebarDark"));
-            ($("#MenuImg").attr("src","./assets/logo-light.svg"));
-            ($("#headDiv").attr("id","headerDiv"));
-            ($("#selectTheme").attr("id","darkTheme"));
-
+            $("#MenuImg").attr("src","./assets/logo-light.svg");
+            $("#header").css("background","#2b2c37");
+            $("#slidebar").css("background","#2b2c37");
+            $("#mainContent").css("background","#20212c");
+            $("#headDiv").css("border-left","1px solid #828fa3");
+            $("#selectTheme ").css("background-color","#20212c");
+            $('#headTitle').css("color","white");
+            $('.taskTitle').css("color","white");
+            $('#circleDiv').css("color","white");
+            $('.taskdiv').css("box-shadow","rgb(0 0 1 / 20%) 1vh 1vh 2vh 0vh")
+            $('.taskdiv').css("background","#2b2c37")
+            $('.addColumnDiv').css("background","#2b2c37")
+            $('.addColumnDiv').css("color","white")
+            $('.columnname').css("color","white")      
+            $('#slidebar').css("border-top","1px solid #828fa3") 
+            $('#mainContent').css("border-left","1px solid #828fa3") 
+            $('#theme').css("border-top","1px solid #828fa3") 
+            $('#mainContent').css("border-top","1px solid #828fa3") 
+            $('#taskscontents').css("background","#2b2c37");
+            $("#menu").attr("class","Menu");
+            $("#editboardselect").attr("class","boardselect");
+            $("#deleteboardselect").attr("class","boardselect");
         }
         else{
-            ($("#WholeContainer").attr("id","whole-container"));
-            ($("#HeaderDark").attr("id","header"));
-            ($("#slidebarDark").attr("id","slidebar"));
-            ($("#MenuImg").attr("src","./assets/logo-dark.svg"));
-            ($("#darkTheme").attr("id","selectTheme"));
+            $("#header").css("background","#ffffff");
+            $("#slidebar").css("background","white");
+            $("#MenuImg").attr("src","./assets/logo-dark.svg");
+            $("#slidebar").css("background","white");
+            $("#headDiv").css("border-left","1px solid #aba4ff");
+            $("#mainContent").css("background","#e4ebfa");
+            $("#selectTheme").css('background-color','#f4f7fd');
+            $('#headTitle').css("color","black");
+            $('.taskTitle').css("color","black");
+            $('#circleDiv').css("color","black");
+            $('.taskdiv').css("box-shadow","rgb(100 100 111 / 20%) 1vh 1vh 2vh 0vh")
+            $('.taskdiv').css("background","white")
+            $('.addColumnDiv').css("background","#eaf0fb")
+            $('.addColumnDiv').css("color","black")
+            $('.columnname').css("color","black")
+            $('#slidebar').css("border-top","1px solid #aba4ff") 
+            $('#mainContent').css("border-left","1px solid #aba4ff") 
+            $('#theme').css("border-top","1px solid #aba4ff")
+            $('#mainContent').css("border-top","1px solid #aba4ff")
+            $('#taskscontents').css("background","white");
+            $("#menu").attr("class","menu");
+            $("#editboardselect").attr("class","BoardSelect");
+            $("#deleteboardselect").attr("class","BoardSelect");
 
         }
     })
   
     $(".hideSideBar").click(function(){
-        $("#slidebar").remove();
-        $("#slidebarDark").remove();
+        console.log("hi2")
+        $("#slidebar").hide();
+        $("#slidebarDark").hide();
         $("#Main").attr("id","mainDiv")
+        $('#mainDiv').append("<div id='showslidebar' onclick='show()' class='showslidebar' ><img onclick='show()' class='showimg showslidebar' src='./assets/icon-show-sidebar.svg'/></div>")
     })
 
    
- 
+
 
 });
+function show(){
+    console.log("hiii")
+    $("#slidebar").show();
+    $("#slidebarDark").show();
+    $('#showslidebar').hide();
+    $("#mainDiv").attr("id","Main")
+}
 
 // function sendData(boardColumnDetails){
 //     let tasks="";
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const bodyParser = require('body-parser');
 const path=require('path')
 const express=require('express');
-const port=5009;
+const port=5010;
 const app=express();
-const datajson=require('./data.json')
+const datajson=require('./data.json');
 const database=require('mysql');
 const { connect } = require('http2');
 const { urlencoded } = require('body-parser'); 
+const { compile } = require('ejs');
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 let connectDatabase=database.createConnection({
@@ -21,15 +22,15 @@ if(err) {
     console.log(err);
 }
 else{
-        // var sql="CREATE TABLE boardDetails(boardId int AUTO_INCREMENT PRIMARY KEY, boardName varchar(255), columnName JSON, tasks JSON)";
-        //  var sql="drop table boardDetails"
-      //  connectDatabase.query(sql,(err,result)=>{
-      //       if(err){
-      //         console.log(err)
-      //       }
-      //       else{
-      //         console.log(result);
-      //    }      })
+                // var sql="CREATE TABLE boardDetails(boardId int AUTO_INCREMENT PRIMARY KEY, boardName varchar(255), columnName JSON)";
+                  //  var sql="drop table boardDetails"
+              //  connectDatabase.query(sql,(err,result)=>{
+              //      if(err){
+              //          console.log(err)
+              //        }
+              //        else{
+              //          console.log(result);
+              //     }      })
     
  }
  })
@@ -43,18 +44,79 @@ app.set("view engine","ejs");
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/index',(req,res)=>{
-   var sql="SELECT * FROM boardDetails";
-   connectDatabase.query(sql,(err,result)=>{
-     if(err){
-       console.log(err)
-     }
-     else{
-      //  console.log(result);
-       res.render("index", { boardValue: result });
-     }
-   })
-   
+  var sql="SELECT * FROM boardDetails";
+  connectDatabase.query(sql,(err,result)=>{
+    if(err){
+          console.log(err);
+        }
+        else{
+       res.render("index",{boardValue:result});
+        }
+      })
 
+})
+app.get('/createBoard',(req,res)=>{
+  res.render("createBoard")
+})
+app.get('/newBoard',(req,res)=>{
+  var sql="SELECT * FROM boardDetails";
+  connectDatabase.query(sql,(err,result)=>{
+    if(err){
+          console.log(err);
+        }
+        else{
+       res.render("newBoard",{boardValue:result});
+        }
+      })
+})
+
+app.get('/newBoardPage',(req,res)=>{
+  res.render("index")
+})
+
+
+app.post('/newBoardPage',function(req,res) {
+  var newBoardNameValue=req.body.newboardname;
+  var newBoardColumnCount=req.body.ListColumnCount;
+  let NewBoardColumnArray=[];
+
+  for(let i=1; i<= newBoardColumnCount ; i++ ){
+     NewBoardColumnArray.push(`${req.body["list"+i]}`)
+  }
+  let columnnames=JSON.stringify(NewBoardColumnArray)
+  var sql = `INSERT INTO boardDetails (boardName, columnName) VALUES ('${newBoardNameValue}', '${columnnames}')`;
+      connectDatabase.query(sql,(err,result)=>{
+          if(err){
+                console.log(err);
+              }
+              else{
+                  // console.log(result)
+
+                  var boardtablename=newBoardNameValue.split(" ").join("_");
+                  var boardTable = `CREATE TABLE ${boardtablename}(TASKCOUNT INT AUTO_INCREMENT PRIMARY KEY, Title varchar(255),Description TEXT,Subtasks JSON,status varchar(255))`;
+                  connectDatabase.query(boardTable,(err,result)=>{
+                      if(err){
+                            console.log(err);
+                          }
+                          else{
+                              // console.log(result)
+            
+            
+                               res.redirect('/index')
+             
+                          }
+                       
+                        });
+      
+
+                  //  res.redirect('/index')
+ 
+              }
+           
+            });
+ 
+  
+          
 })
 
 app.get('/header/:boardId',(req,res)=>{
@@ -76,88 +138,79 @@ app.get('/header/:boardId',(req,res)=>{
      
        });
 })
+app.get('/mainContent/:boardId',(req,res)=>{
+  let idValue=req.params.boardId;    
+var sql=`SELECT * FROM boardDetails WHERE boardId=${idValue}`;
+//  let boardValue;
+connectDatabase.query(sql,(err,result)=>{
+ if(err){
+   console.log(err)
+ }
+ else{
+   // console.log(result);
+   let tasksvalue=result;
+    var showtablename=(tasksvalue[0].boardName).split(" ").join("_");
+    var sql=`SELECT * FROM ${showtablename}`;
+    //  let boardValue;
+    connectDatabase.query(sql,(err,result)=>{
+     if(err){
+       console.log(err)
+     }
+     else{
+     var taskdetailsvalue=result;
+     console.log(taskdetailsvalue)
+   res.render("mainContent", { boardValue: tasksvalue,taskdetailsvalues:taskdetailsvalue });
+     }
+    })
+ }
+})
+
+})
+app.get('/newColumn/:boardid',(req,res)=>{
+  var boardidvalueno=req.params.boardid;
+  res.render("newColumn",{boardidvalueno});
+})
+
 app.get('/deleteBoard/:boardName',(req,res)=>{
-  let deleteboardname=req.params.boardName;
+  let deleteboardname=(req.params.boardName).split("*").join(" ");
+  console.log(deleteboardname)
             res.render("deleteBoard",{deleteboardname});
     })
 
- app.get('/mainContent/:boardId',(req,res)=>{
-         let idValue=req.params.boardId;    
-      var sql=`SELECT * FROM boardDetails WHERE boardId=${idValue}`;
-     //  let boardValue;
-      connectDatabase.query(sql,(err,result)=>{
-        if(err){
-          console.log(err)
-        }
-        else{
-          // console.log(result);
-          res.render("mainContent", { boardValue: result });
-        }
-      })
 
-  })
-   
-app.get('/newBoard',(req,res)=>{
+    app.get('/createColumn/:columnboardid',(req,res)=>{
+      var createcolumnboard=`SELECT * FROM boardDetails WHERE boardId=${req.params.columnboardid}`;
+      connectDatabase.query(createcolumnboard,(err,result)=>{
+       if(err){
+         console.log(err)
+       }
+       else{
+         console.log(result);
+         res.render("createColumn",{columnresult:result[0]});
+       }
+     })
+   })
 
-  var sql="SELECT * FROM boardDetails";
-  connectDatabase.query(sql,(err,result)=>{
+
+   app.post('/addnewcolumn/:boardidnovalue',(req,res)=>{
+    console.log(req.params.boardidnovalue)
+    console.log(req.body.newcolumnobject)
+    var columnarrays=[];
+    var newcolumnarray=req.body.newcolumnobject;
+    columnarrays.push(newcolumnarray)
+  var columnadd=`UPDATE boardDetails SET columnName='${columnarrays}' WHERE boardId=${req.params.boardidnovalue}`;
+  connectDatabase.query(columnadd,(err,result)=>{
     if(err){
       console.log(err)
     }
     else{
-      // console.log(result);
- 
-      res.render("newBoard",{boardValue:result});
-    
-  
+     console.log(result);
     }
   })
 
-})
-app.get('/newBoardPage',(req,res) =>{
-res.render("index");
-})
-app.post('/newBoardPage',function(req,res) {
-  // console.log(req.body.newboardname)
-  var newBoardNameValue=req.body.newboardname;
-  var newBoardColumnCount=req.body.ListColumnCount;
-  // console.log(newBoardColumnCount);
-  let NewBoardColumnArray=[]
+  })
 
-  for(let i=1; i<= newBoardColumnCount ; i++ ){
-     NewBoardColumnArray.push(`${req.body["list"+i]}`)
-  }
-  let columnnames=JSON.stringify(NewBoardColumnArray)
-  var sql = `INSERT INTO boardDetails (boardName, columnName, tasks) VALUES ('${newBoardNameValue}', '${columnnames}','[]')`;
-      connectDatabase.query(sql,(err,result)=>{
-          if(err){
-                console.log(err);
-              }
-              else{
-                // console.log(result)
 
-                var sql="SELECT * FROM boardDetails";
-                connectDatabase.query(sql,(err,result)=>{
-                  if(err){
-                    console.log(err)
-                  }
-                  else{
-                    // console.log(result);
-                    res.redirect("/index");
-                    res.render("index", { boardValue: result });
-                  }
-                })
-              }
-           
-            });
- 
-  
- 
-
-})
-app.get("/createBoard",(req,res)=>{
-  res.render("createBoard");
- })
 
  app.get("/newTask/:boardNamevalue",(req,res)=>{
   const boardnamevalue=req.params.boardNamevalue;
@@ -173,96 +226,49 @@ app.get("/createBoard",(req,res)=>{
     else{
       // console.log(resultvalue);
       //  console.log(JSON.parse(resultvalue[0].columnName))
-       res.render("newTask",{columnArrayvalue:JSON.parse(resultvalue[0].columnName),boardidno:resultvalue[0].boardId});
+       res.render("newTask",{columnArrayvalue:JSON.parse(resultvalue[0].columnName),boardidno:resultvalue[0].boardId,boardname:resultvalue[0].boardName});
     }
   })
  })
 
- app.post('/newTaskPage/:boardidnovalue',function(req,res){
-  // console.log(req.body.tasktitle);
-  // console.log(req.body.description);
-  // console.log(req.body.subtaskListCountvalue);
-  // console.log(req.params.boardidnovalue);
-  // console.log(req.body.columnname)
-  let newsubtaskcount=req.body.subtaskListCountvalue;
-  let NewBoardTaskArray=[];
-
-  for(let i=1; i<= newsubtaskcount ; i++ ){
-    NewBoardTaskArray.push({tasks:`${req.body["subtasklist"+i]}`,isCompleted:false})
-  }
-  let subtasknames=JSON.stringify(NewBoardTaskArray);
-   let taskvaluesafter;
-   var tasksaftervalue=`SELECT * FROM boardDetails WHERE boardId=${req.params.boardidnovalue}`;
-   connectDatabase.query(tasksaftervalue,(err,result)=>{
+ app.post('/addnewtask/:boardname',(req,res)=>{
+  console.log(req.params.boardname)
+  var inserttablename=(req.params.boardname).split(" ").join("_");
+  console.log(inserttablename)
+  // console.log(req.body.newtaskobject)
+  var newtaskobject=JSON.parse(req.body.newtaskobject);
+  console.log(newtaskobject)
+  console.log(newtaskobject.title)
+   var sql=`insert into ${inserttablename} (Title,Description,Subtasks,status) values ('${newtaskobject.title}','${newtaskobject.description}','${JSON.stringify(newtaskobject.subtasks)}','${newtaskobject.status}') `
+   connectDatabase.query(sql,(err,result)=>{
     if(err){
       console.log(err)
     }
     else{
-      //  console.log(result[0].tasks);
-       taskvaluesafter=result[0].tasks;
-       
+      
     }
-  })
-let tasksArray=[];
-
-   let tasksObject={
-    title:`${req.body.tasktitle}`,
-    description:`${req.body.description}`,
-    subtasks:`${subtasknames}`,
-    status:`${req.body.columnname}`,
-   }
-   console.log(tasksObject);
-if(taskvaluesafter!=[]  ){
-tasksArray.push(taskvaluesafter);
+   })
  
-}
-console.log("gfhj")
-console.log(tasksaftervalue)
-console.log("gfhj")
-
-tasksArray.push(tasksObject);
-let taskArrayvalue=JSON.stringify(tasksArray);
-console.log(tasksArray);
-
-// var taskadd=`UPDATE boardDetails SET tasks='${taskArrayvalue}' WHERE boardId=${req.params.boardidnovalue}`;
-// connectDatabase.query(taskadd,(err,result)=>{
-//   if(err){
-//     console.log(err);
-//   }
-//   else{
-//     // console.log(result);
-//   }
-// })
-  var sql="SELECT * FROM boardDetails";
-  connectDatabase.query(sql,(err,result)=>{
-    if(err){
-      console.log(err);
-    }
-    else{
-      // console.log(result);
-      res.redirect("/index")
-      // res.render("index", { boardValue: result });
-    }
   })
- })
+ 
+  app.get('/task/:currboardnamevalue/:taskcount/:boardno',(req,res)=>{
+    console.log(req.params.currboardnamevalue)
+    console.log(req.params.taskcount);
+    var taskvalue=`SELECT * FROM ${req.params.currboardnamevalue} WHERE TASKCOUNT=${req.params.taskcount}`;
+    connectDatabase.query(taskvalue,(err,result)=>{
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log(result)
+        res.render("task",{taskvalues:result,boardidnumber:req.params.boardno,currboardname:req.params.currboardnamevalue})
+      }
+    })
+  })
 
 
-  // app.get('/newBoard',(req,res)=>{
-  //   var sql="SELECT * FROM boardDetails";
-  //   connectDatabase.query(sql,(err,result)=>{
-  //     if(err){
-  //       console.log(err)
-  //     }
-  //     else{
-  //       console.log(result);
-  //       res.render("index", { boardValue: result });
-  //     }
-  //   })
-  // })
   app.post("/deleteBoardDetails/:boardnamevalue",function(req,res){
-    console.log("ghj")
      console.log(req.params.boardnamevalue);
-    console.log("ghj")
      var deleteBoardrow=`DELETE FROM boardDetails WHERE boardName='${req.params.boardnamevalue}'`;
      connectDatabase.query(deleteBoardrow,(err,result)=>{
            if(err){
@@ -270,6 +276,16 @@ console.log(tasksArray);
            }
            else{
              console.log(result);
+
+             var deletetable=`DROP TABLE ${(req.params.boardnamevalue).split(" ").join("_")}`;
+             connectDatabase.query(deletetable,(err,result)=>{
+              if(err){
+                console.log(err);
+              }
+              else{
+
+              }
+             })
      res.redirect("/index");
 
            }
@@ -277,4 +293,123 @@ console.log(tasksArray);
 
   })
 
+  app.get('/editBoard/:boardnames',(req,res)=>{
+    var currentboardname=req.params.boardnames
+    currentboardname=currentboardname.split("*").join(" ");
+    var columnresultquery=`SELECT * FROM boardDetails WHERE boardName='${currentboardname}'`
+    connectDatabase.query(columnresultquery,(err,result)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+       res.render("editBoard",{columnresult:result})
+      
+      }
+    })
+
+  })
+
+  app.post('/editBoard/:boardno',(req,res)=>{
+    console.log((req.body.neweditcolumnobject).columnarrayvalue)
+    // var newBoardColumnCount=req.body.ListColumnCount;
+    // let NewBoardColumnArray=[];
+  
+    // for(let i=1; i<= newBoardColumnCount ; i++ ){
+    //    NewBoardColumnArray.push(`${req.body["list"+i]}`)
+    // }
+    var editboardcommand=`UPDATE boardDetails SET boardName='${(req.body.neweditcolumnobject).boardnamevalue}', columnName='${JSON.stringify((req.body.neweditcolumnobject).columnarrayvalue)}' WHERE boardId=${req.params.boardno}`;
+    connectDatabase.query(editboardcommand,(err,result)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+
+      }
+    })
+  })
+  app.get('/editTask/:BoardNo/:taskcount',(req,res)=>{
+
+    console.log(req.params.BoardNo)
+    console.log(req.params.taskcount)
+    var edittaskquery=`SELECT * FROM boardDetails WHERE boardId=${req.params.BoardNo}`;
+    connectDatabase.query(edittaskquery,(err,result)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log(result[0].boardName);
+        var boardsplitname=(result[0].boardName).split(" ").join("_");
+        var coulmnnames=result[0].columnName;
+        var collecttaskdetails=`SELECT * FROM ${boardsplitname} WHERE TASKCOUNT=${req.params.taskcount}`;
+        connectDatabase.query(collecttaskdetails,(err,result)=>{
+          if(err){
+            console.log(err)
+          }
+          else{
+            res.render("editTask",{taskdetails:result[0],columndetails:coulmnnames,boardname:boardsplitname,taskcount:req.params.taskcount,boardno:req.params.BoardNo})
+
+          }
+        })
+      }
+    })
+  })
+  app.post('/editnewtask/:boardname/:taskcount',(req,res)=>{
+
+    console.log(req.params.boardname)
+    console.log(req.params.taskcount)
+    console.log(JSON.parse(req.body.newtaskobject).title);
+    var newtaskobject=JSON.parse(req.body.newtaskobject);
+    var sql=`UPDATE ${req.params.boardname} SET Title='${newtaskobject.title}',Description='${newtaskobject.description}',Subtasks='${JSON.stringify(newtaskobject.subtasks)}',status='${newtaskobject.status}' WHERE TASKCOUNT=${req.params.taskcount} `
+    connectDatabase.query(sql,(err,result)=>{
+     if(err){
+       console.log(err)
+     }
+     else{
+       
+     }
+    })
+  })
+
+  app.get('/deleteTask/:boardname/:taskcount/:boardidnumber',(req,res)=>{
+    console.log(req.body.boardname);
+    console.log(req.body.taskcount);
+    var sql=`SELECT * FROM ${req.params.boardname} WHERE TASKCOUNT=${req.params.taskcount}`;
+    connectDatabase.query(sql,(err,result)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+    res.render("deleteTask",{tasktitle:result[0].Title,taskcountvalue:req.params.taskcount,boardName:req.params.boardname,boardidnumber:req.params.boardidnumber})
+                               
+      }
+    })
+  })
+app.get('/deleteTaskDetails/:boardName/:taskcount',(req,res)=>{
+  res.redirect("/index")
+})
+  app.post('/deleteTaskDetails/:boardName/:taskcount',(req,res)=>{
+    var sql=`DELETE FROM ${req.params.boardName} WHERE TASKCOUNT=${req.params.taskcount}`;
+    connectDatabase.query(sql,(err,result)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+           res.redirect("/index")
+      }
+    })
+  })
+
+  app.post('/addnewsubtaskvalue/:boardname/:taskcount',(req,res)=>{
+    var editquery=`UPDATE ${req.params.boardname} SET Subtasks='${req.body.newtaskobject}' WHERE TASKCOUNT=${req.params.taskcount}`
+    connectDatabase.query(editquery,(err,result)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+
+      }
+    })
+  
+  
+  })
  app.listen(port,()=>console.log(port));
